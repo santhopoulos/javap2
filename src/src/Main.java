fdae445483a14f6cb0e671a84857a52e747a3709
Stratos <anthopoue@csd.auth.gr>
2021-12-27 17:57:06 +0200

    initial cpmmit

---
+++ src/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+
+    public static void main (String[] args){
+        System.out.println("Building a Java project and connecting it with git from scratch");
+        System.out.println("In the second part of the project we will be using JAvaSwing for the graphical part as well as JUnit for running tests");
+    }
+
+
+}
